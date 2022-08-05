d40abaa80399316ebe4802a1ef5b5ffc668548c7
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-05 12:25:03 +0530

    Add files via upload

---
+++ Reorder List Question (L-143).java
@@ -0,0 +1,30 @@
+class Solution {
+    public void reorderList(ListNode head) {
+      if(head== null) return;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode mid = slow;
+        ListNode previous = null, current = slow, temp;
+        while(current != null){
+            temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        ListNode first = head, second = previous;
+        while(second.next != null){
+            temp = first.next;
+            first.next = second;
+            first = temp;
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+    }
+}
